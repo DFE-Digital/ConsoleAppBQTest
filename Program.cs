a9158f1f4c9154fafdd6a70feeb1cceb4a00c0d0
Mahesh KUNIGIRI <Mahesh.KUNIGIRI@EDUCATION.GOV.UK>
2023-11-09 18:17:14 +0000

    "274-ms10-transfer-crm-fields-into-big-query" changes

--- Program.cs
+++ Program.cs
@@ -41,12 +41,13 @@ try
     InsertApplicationRecordsToBQInBatches(builder, client, datasetId);
     InsertProfileRecordsToBQInBatches(builder, client, datasetId);
     InsertSchoolExperienceRecordsToBQInBatches(builder, client, seDatasetId, seTableName);
+    InsertCandidateWorkexpErienceRecordsToBQInBatches(builder, client, datasetId);
+
 }
 catch (Exception ex)
 {
     LogErrorMessage(ex.ToString());    
 }
-
 
 void DeleteDataFromBQTables(BigQueryClient client, string dataSetId, string seDataSetId, string seTableName)
 {    
@@ -92,7 +93,13 @@ void DeleteDataFromBQTables(BigQueryClient client, string dataSetId, string seDa
     sql = $"TRUNCATE TABLE `get-into-teaching.{seDataSetId}.{seTableName}`";
     LogMessage($"School Experience Requests Records delete - SQL:{sql}");
     client.ExecuteQuery(sql, parameters);
-    LogMessage($"School Experience Requests Records deleted - End Time:{DateTime.Now.ToString()}");    
+    LogMessage($"School Experience Requests Records deleted - End Time:{DateTime.Now.ToString()}");
+
+    //sql = @"TRUNCATE TABLE `get-into-teaching.transactions.work_experience`";
+    sql = $"TRUNCATE TABLE `get-into-teaching.{dataSetId}.work_experience`";
+    LogMessage($"work_experience Records delete - SQL:{sql}");
+    client.ExecuteQuery(sql, parameters);
+    LogMessage($"Candidate Work experience Records deleted - End Time:{DateTime.Now.ToString()}");
 }
 
 //Batches code
@@ -303,6 +310,10 @@ static void InsertMLSubscriptionRecordsToBQInBatches(SqlConnectionStringBuilder 
                     if (row.ItemArray[7] != null && row.ItemArray[7] != System.DBNull.Value)
                     {
                         insert.Add("opted_out_of_post", Convert.ToBoolean(row.ItemArray[7]));
+                    }
+                    if (row.ItemArray[8] != null && row.ItemArray[8] != System.DBNull.Value)
+                    {
+                        insert.Add("unsubscribe_reason", row.ItemArray[8].ToString());
                     }
 
                     rows.Add(insert);
@@ -593,6 +604,101 @@ static void InsertProfileRecordsToBQInBatches(SqlConnectionStringBuilder builder
                     {
                         insert.Add("consideration_stage", row.ItemArray[24].ToString());
                     }
+                    //start new fields 
+
+                    if (row.ItemArray[25] != null && row.ItemArray[25] != System.DBNull.Value)
+                    {
+                        insert.Add("candidate_type", row.ItemArray[25].ToString());
+                    }
+                    //TODO this is not bool
+                    //if (row.ItemArray[26] != null && row.ItemArray[26] != System.DBNull.Value)
+                    //{
+                    //    insert.Add("is_candidate_eligible_for_an_adviser", row.ItemArray[26].ToString());
+                    //}
+                    if (row.ItemArray[27] != null && row.ItemArray[27] != System.DBNull.Value)
+                    {
+                        insert.Add("adviser_status", row.ItemArray[27].ToString());
+                    }
+
+                    if (row.ItemArray[28] != null && row.ItemArray[28] != System.DBNull.Value)
+                    {
+                        insert.Add("adviser_status_reason", row.ItemArray[28].ToString());
+                    }
+                    if (row.ItemArray[29] != null && row.ItemArray[29] != System.DBNull.Value)
+                    {
+                        insert.Add("re_register_status", row.ItemArray[29].ToString());
+                    }
+                    if (row.ItemArray[30] != null && row.ItemArray[30] != System.DBNull.Value)
+                    {
+                        insert.Add("current_adviser", row.ItemArray[30].ToString());
+                    }                   
+
+                    if (row.ItemArray[31] != null && row.ItemArray[31] != System.DBNull.Value)
+                    {
+                        insert.Add("current_adviser_team", row.ItemArray[31].ToString());
+                    }
+
+                    if (row.ItemArray[32] != null && row.ItemArray[32] != System.DBNull.Value)
+                    {
+                        insert.Add("previous_adviser", row.ItemArray[32].ToString());
+                    }
+                    if (row.ItemArray[33] != null && row.ItemArray[33] != System.DBNull.Value)
+                    {
+                        insert.Add("date_assigned_to_previous_adviser", Convert.ToDateTime(row.ItemArray[33]).ToString("yyyy-MM-dd"));
+                    }
+                    if (row.ItemArray[34] != null && row.ItemArray[34] != System.DBNull.Value)
+                    {
+                        insert.Add("date_released", Convert.ToDateTime(row.ItemArray[34]).ToString("yyyy-MM-dd"));
+                    }
+                    if (row.ItemArray[35] != null && row.ItemArray[35] != System.DBNull.Value)
+                    {
+                        insert.Add("postcode", row.ItemArray[35].ToString());
+                    }
+                    if (row.ItemArray[36] != null && row.ItemArray[36] != System.DBNull.Value)
+                    {
+                        insert.Add("right_to_study_in_the_uk", row.ItemArray[36].ToString());
+                    }
+                    if (row.ItemArray[37] != null && row.ItemArray[37] != System.DBNull.Value)
+                    {
+                        insert.Add("preferred_training_region", row.ItemArray[37].ToString());
+                    }
+                    if (row.ItemArray[38] != null && row.ItemArray[38] != System.DBNull.Value)
+                    {
+                        insert.Add("preferred_teaching_course", row.ItemArray[38].ToString());
+                    }
+                    if (row.ItemArray[39] != null && row.ItemArray[39] != System.DBNull.Value)
+                    {
+                        insert.Add("preferred_education_phase", row.ItemArray[39].ToString());
+                    }
+                    //TODO this is not bool
+                    //if (row.ItemArray[40] != null && row.ItemArray[40] != System.DBNull.Value)
+                    //{
+                    //    insert.Add("has_GCSE_english", Convert.ToBoolean(row.ItemArray[40]));
+                    //}
+                    //TODO this is not bool
+                    //if (row.ItemArray[41] != null && row.ItemArray[41] != System.DBNull.Value)
+                    //{
+                    //    insert.Add("has_GCSE_maths", Convert.ToBoolean(row.ItemArray[41]));
+                    //}
+                    //TODO this is not bool
+                    //if (row.ItemArray[42] != null && row.ItemArray[42] != System.DBNull.Value)
+                    //{
+                    //    insert.Add("has_GCSE_science", Convert.ToBoolean(row.ItemArray[42]));
+                    //}
+                    if (row.ItemArray[43] != null && row.ItemArray[43] != System.DBNull.Value)
+                    {
+                        insert.Add("has_dbs_certificate", Convert.ToBoolean(row.ItemArray[43]));
+                    }
+                    if (row.ItemArray[44] != null && row.ItemArray[44] != System.DBNull.Value)
+                    {
+                        insert.Add("issue_date_of_DBS_certificate", Convert.ToDateTime(row.ItemArray[44]).ToString("yyyy-MM-dd"));
+                    }
+                    if (row.ItemArray[45] != null && row.ItemArray[45] != System.DBNull.Value)
+                    {
+                        insert.Add("days_since_triaged", row.ItemArray[45]);
+                    }
+
+                    // new fields end
                     rows.Add(insert);
                     counter++;
                     if (counter % 1000 == 0 || counter == totalRecordsCount)
@@ -604,7 +710,7 @@ static void InsertProfileRecordsToBQInBatches(SqlConnectionStringBuilder builder
                 catch (Exception ex)
                 {
                     errorCount++;
-                    LogMessage($"Error:{errorCount} - Id: {row.ItemArray[0].ToString()}");
+                    LogMessage($"Error:{errorCount} - Id: {row.ItemArray[0].ToString()} - Error Message:{ex.Message}");
                 }
             }
 
@@ -690,7 +796,69 @@ static void InsertSchoolExperienceRecordsToBQInBatches(SqlConnectionStringBuilde
     }
     LogMessage($"School Experience Records - EndTime:{DateTime.Now.ToString()}");
 }
+ 
+static void InsertCandidateWorkexpErienceRecordsToBQInBatches(SqlConnectionStringBuilder builder, BigQueryClient client, string dataSetId)
+{
+    LogMessage($"Candidate Workexp Erience Records - StartTime:{DateTime.Now.ToString()}");
+    var dataSet = DataLogic.GetCandidateWorkexpErienceFromCRM(builder.ConnectionString);
+    if (dataSet != null && dataSet.Tables.Count > 0)
+    {
+        int errorCount = 0;
+        int counter = 0;
+        int totalRecordsCount = dataSet.Tables[0].Rows.Count;
+        List<BigQueryInsertRow> rows = new List<BigQueryInsertRow>();
+        foreach (DataRow row in dataSet.Tables[0].Rows)
+        {
+            if (row is not null)
+            {
+                try
+                {
+                    var insert = new BigQueryInsertRow();
+                    if (row.ItemArray[0] != null && row.ItemArray[0] != System.DBNull.Value)
+                    {
+                        insert.Add("contact_id", row.ItemArray[0].ToString());
+                    }
+                    if (row.ItemArray[1] != null && row.ItemArray[1] != System.DBNull.Value)
+                    {
+                        insert.Add("job_title", row.ItemArray[1].ToString());
+                    }
+                    if (row.ItemArray[2] != null && row.ItemArray[2] != System.DBNull.Value)
+                    {
+                        insert.Add("organisation", row.ItemArray[2].ToString());
+                    }
+                    if (row.ItemArray[3] != null && row.ItemArray[3] != System.DBNull.Value)
+                    {
+                        insert.Add("country", row.ItemArray[3].ToString());
+                    }
+                    if (row.ItemArray[4] != null && row.ItemArray[4] != System.DBNull.Value)
+                    {
+                        insert.Add("start_year", row.ItemArray[4].ToString());
+                    }
+                    if (row.ItemArray[5] != null && row.ItemArray[5] != System.DBNull.Value)
+                    {
+                        insert.Add("end_year", row.ItemArray[5].ToString());                       
+                    }
+                   
+                    rows.Add(insert);
+                    counter++;
+                    if (counter % 1000 == 0 || counter == totalRecordsCount)
+                    {
+                        var results = client.InsertRows(dataSetId, "work_experience", rows.ToArray());
+                        rows.Clear();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errorCount++;
+                    LogMessage($"Error:{errorCount} - Id: {row.ItemArray[0].ToString()}");
+                }
+            }
 
+        }
+        LogMessage($"Candidate Workexp Erience Records - Total Records Processed:{counter}");
+    }
+    LogMessage($"Candidate Workexp Erience Records - EndTime:{DateTime.Now.ToString()}");
+}
 
 static void LogMessage(string message)
 {
